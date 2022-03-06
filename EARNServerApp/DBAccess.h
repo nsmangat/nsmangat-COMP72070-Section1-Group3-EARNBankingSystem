875d9a45f90661b5998326210c9a6f4a7a087cba
Anthony Sasso <71456585+anthonysasso2001@users.noreply.github.com>
2022-03-06 17:05:29 -0500

    Finished more work on the header, need to make some changes...

--- EARNServerApp/DBAccess.h
+++ EARNServerApp/DBAccess.h
@@ -8,54 +8,57 @@
 
 #include "DBObjects.h"
 
-using namespace std;
+namespace EarnDB {
+	using namespace std;
 
-class DBAccessObject {
-	int objectID;
-public:
-	DBAccessObject(DBAccessObject& copyObject);
-	DBAccessObject(int newObjectID);
+	//virtual access interface for DB
+	class DBAccessInterface {
+		int objectID;
 
-	virtual string addInfoToDB() = 0;
-	virtual string modifyInfoInDB() = 0;
-	virtual string deleteInfoInDB() = 0;
+	public:
+		DBAccessInterface();
+		DBAccessInterface(const DBAccessInterface& copyAccessObject);
+		DBAccessInterface(int newObjectID);
 
-	int getObjectID();
-};
+		virtual string addInfoToDB() = 0;
+		virtual string modifyInfoInDB() = 0;
+		virtual string deleteInfoInDB() = 0;
 
-//enummeration for different types of DBObjects
-enum DBOType{ ACCOUNT, CLIENT, TRANSACTION};
+		int getObjectID();
+	};
 
-class DBReader{
-	//subfunctions for checkIDExists
-	bool checkClientIDExists(int checkClientID);
-	bool checkAccountIDExists(int checkAccountID);
-	bool checkTransactionIDExists(int checkTransactionID);
+	class DBReader {
+		//subfunctions for checkIDExists
+		bool checkClientIDExists(int checkClientID);
+		bool checkAccountIDExists(int checkAccountID);
+		bool checkTransactionIDExists(int checkTransactionID);
 
-public:
-	//get single objects
-	bool getObjectInfo(int objectID, DBClient& copyClient);
-	bool getObjectInfo(int objectID, DBAccount& copyAccount);
-	bool getObjectInfo(int objectID, DBTransaction& copyTransaction);
+	public:
+		//get single objects
+		bool getObjectInfo(int objectID, DBClient& copyClient);
+		bool getObjectInfo(int objectID, DBAccount& copyAccount);
+		bool getObjectInfo(int objectID, DBTransaction& copyTransaction);
 
-	//get all object from a table related (all clients, accounts of client, and invoices for account
-	bool getObjectsInfo(int& numOfClients, vector<DBClient>& clientsVec);
-	bool getObjectsInfo(int clientID, int& numOfAccounts, vector<DBAccount>& clientAccountsVec);
-	bool getObjectsInfo(int accountID, int& numOfTransactions, vector<DBTransaction>& accountTransactionsVec);
+		//get all object from a table related (all clients, accounts of client, and invoices for account
+		bool getObjectsInfo(int& numOfClients, vector<DBClient>& clientsVec);
+		bool getObjectsInfo(int clientID, int& numOfAccounts, vector<DBAccount>& clientAccountsVec);
+		bool getObjectsInfo(int accountID, int& numOfTransactions, vector<DBTransaction>& accountTransactionsVec);
 
-	//check if id exists... with sub functions for each option
-	bool checkIDExists(int checkID, DBOType idType);
-};
+		//check if id exists... with sub functions for each option
+		bool checkIDExists(int checkID, DBOType idType);
+	};
 
-class DBValidation{
-	int validateClient(string usernameOrNum, string passwordHash);
-public:
-	DBClient clientLogin(string usernameORNum, string passwordHash);
-};
+	class DBValidation {
+		int validateClient(string usernameOrNum, string passwordHash);
 
-class DBWriter{
-public:
-	bool addObject(DBObject inputObj);
-	bool modifyObjectInfo(DBObject inputObj);
-	bool deleteObject(DBObject inputObj);
-};
+	public:
+		DBClient clientLogin(string usernameORNum, string passwordHash);
+	};
+
+	class DBWriter {
+	public:
+		bool addObject(DBObject inputObj);
+		bool modifyObjectInfo(DBObject inputObj);
+		bool deleteObject(DBObject inputObj);
+	};
+}
--- EARNServerApp/DBObjects.h
+++ EARNServerApp/DBObjects.h
@@ -1,40 +1,120 @@
 #pragma once
 #include <string>
+#include <time.h>
+
 #include "DBAccess.h"
 
-#define VARCHAR_LENGTH 45;
-#define ZIP_LENGTH 6;
+namespace EarnDB {
+	using namespace std;
+	
+	const int VARCHARLEN = 45;	//length of varchars, so we don't overwrite in database
+	const int ZIPLEN = 6;		//length of zip codes, since they are always the same
 
-using namespace std;
+	//enummeration for different types of DBObjects
+	enum DBOType {CLIENT, ACCOUNT, TRANSACTION };
 
-class DBObject :DBAccessObject {
-	DBOType objectType;
-public:
-	DBObject(DBOType initalizeType, int objectID);
+	class DBObject : public DBAccessInterface {
+		DBOType objectType;
 
-	DBOType getDBType();
-	virtual string displayInfo() = 0;
-};
+	public:
+		DBObject();	//empty constructor
+		DBObject(const DBObject& copyObject) : DBAccessInterface((const DBAccessInterface&) copyObject);	//constructor casts input to parent type to send up to it's constructor
+		DBObject(DBOType initalizeType, int objectID) :DBAccessInterface((const int) objectID);	//parametrized constructor also passes objectID up
 
-class DBClient :DBObject {
-	string firstName;
-	string lastName;
-	string email;
-	string phoneNumber;
-	string street;
-	string city;
-	string province;
-	string zipcode;
-public:
-	DBClient(DBClient& copyClient);
-	string displayInfo();
+		DBOType getDBType();
+		virtual string displayInfo() = 0;
+	};
 
-};
+	struct DBClientInfo {
+			char firstName[VARCHARLEN];
+			char lastName[VARCHARLEN];
+			char email[VARCHARLEN];
+			char phoneNumber[VARCHARLEN];
+			char street[VARCHARLEN];
+			char city[VARCHARLEN];
+			char province[VARCHARLEN];
+			char zipcode[ZIPLEN];
+		};
 
-class DBAccount :DBObject {
+	class DBClient :DBObject {
+		DBClientInfo clientInfo;
 
-};
+	public:
+		DBClient();
+		DBClient(const DBClient& copyClient): DBObject((const DBObject&)copyClient);
+		DBClient(int inputobjectID, char inputFirst[VARCHARLEN], char inputLast[VARCHARLEN], char inputEmail[VARCHARLEN], char inputPhone[VARCHARLEN], char inputStreet[VARCHARLEN], char inputCity[VARCHARLEN], char inputProvince[VARCHARLEN], char inputZip[ZIPLEN]) :DBObject(CLIENT, inputobjectID);
+		
+		/*
+		Inherited functions from DBObject & Above
+		*/
 
-class DBTransaction: DBObject{
+		string displayInfo();
+		string getLogData();
+		string addInfoToDB();
+		string modifyInfoInDB();
+		string deleteInfoInDB();
+	};
 
-};
+	//enummeration for Account types
+	enum DBAType{CHEQUINGS, SAVINGS};
+
+	struct DBAccountInfo {
+		int clientID;
+		DBAType accountType;
+		double accountBalance;
+	};
+
+	class DBAccount : public DBObject {
+		DBAccountInfo accountInfo;
+
+	public:
+		DBAccount();
+		DBAccount(const DBAccount& copyAccount): DBObject((DBObject&) copyAccount);
+		DBAccount(int inputObjectID, int inputClientID, DBAType inputAccountType, double inputBalance) : DBObject(ACCOUNT, inputObjectID);
+
+		DBAType getAccountType();
+
+		/*
+		Inherited functions from DBObject & Above
+		*/
+
+		string displayInfo();
+		string getLogData();
+		string addInfoToDB();
+		string modifyInfoInDB();
+		string deleteInfoInDB();
+	};
+
+	//enummeration for transaction types
+	enum DBTType{ETRANSFER, CHEQUE, WITHDRAW, ACCOUNTTRANSFER};
+
+	struct DBTransactionInfo {
+			int accountID;
+			DBTType transactionType;
+			char* timeOfTransaction;
+			double previousBalance;
+			double newBalance;
+			int secondaryAccount;
+		};
+
+	class DBTransaction : public DBObject {
+		DBTransactionInfo transactionInfo;
+
+	public:
+		DBTransaction();
+		DBTransaction(DBTransaction& copyTransaction) :DBObject((DBObject&)copyTransaction);
+		DBTransaction(int inputObjectID, int inputAccountID, DBTType inputTransactionType, char* timeOfTransaction, double inputPreviousBalance, double inputNewBalance, int inputSecondaryAccount) :DBObject(TRANSACTION, inputObjectID);
+
+		DBTType getTransactionType();
+
+		/*
+		Inherited functions from DBObject & Above
+		*/
+
+		string displayInfo();
+		string getLogData();
+		string addInfoToDB();
+		string modifyInfoInDB();
+		string deleteInfoInDB();
+	};
+}
